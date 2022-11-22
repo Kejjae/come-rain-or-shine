8be2628122b32caf09b2812a28aa7a5ff227dc9f
Pongwarin00 <56549673+Pongwarin00@users.noreply.github.com>
2022-11-22 19:28:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import tkinter as tk
+import requests #pip install requests
+from PIL import ImageTk, Image #pip install pillow
 
+root = tk.Tk()
+
+root.title("Weather App") #Title
+root.geometry("600x500") #Window size
+
+
+def format_response(weather):
+    try:
+        city = weather['name']
+        condition = weather['weather'][0]['description']
+        tempF = weather['main']['temp']
+        tempC = round((int(tempF) - 32) * 0.5556)
+        final_str = 'City: %s\nConditions: %s\nTemperature: %s °C'%(city, condition, tempC)
+    except:
+        final_str = 'There was a problem retrieving that information'
+    return final_str
