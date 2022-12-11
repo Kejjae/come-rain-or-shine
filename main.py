0530e80b3dcdb275c0c89586b5f98bc80f919ddc
bncnkktn <109953365+bncnkktn@users.noreply.github.com>
2022-12-11 16:44:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -33,3 +33,30 @@ try:
     def on_enter(e):
         """on enter"""
         city1.delete(0, 'end')
+    
+    def on_leave(e):
+        """on leave"""
+        if city1.get() == '':
+            city1.insert(0, 'Search City')
+
+    city1 = Entry(window, width=21, fg='white', bg="#353535", border=0)
+    city1.config(font=('Goudy Old Style', 16))
+    city1.bind("<FocusIn>", on_enter)
+    city1.bind("<FocusOut>", on_leave)
+    city1.insert(0, 'Search City')
+    city1.place(x=540, y=12)
+
+    # date
+
+    timer = datetime.today().strftime('%B')
+    up_timer = (timer.upper())
+    ddate = datetime.now().month
+
+    now = datetime.now()
+
+    cel = now.strftime('%B')
+    month = cel[0:3]
+
+    today = datetime.today()
+
+    date = today.strftime("%d")
