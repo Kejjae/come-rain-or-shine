996d4e29e7c6fd3aeb001c8cb467eda6f9f3966d
Kejjae <118044723+Kejjae@users.noreply.github.com>
2022-12-11 16:34:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,3 @@ window.iconbitmap('ok.ico')
 window.title("Weather")
 window.resizable(0,0)
 
-def format_response(weather):
-    try:
-        city = weather['name']
-        condition = weather['weather'][0]['description']
-        tempF = weather['main']['temp']
-        tempC = round((int(tempF) - 32) * 0.5556)
-        final_str = 'City: %s\nConditions: %s\nTemperature: %s °C'%(city, condition, tempC)
-    except:
-        final_str = 'There was a problem retrieving that information'
-    return final_str
-
-def get_weather(city):
-    #weather_key = 'b86245b23ea848abe010da2d64c71464' อาจเปลี่ยนถ้าขอ api key ทัน
-    #url = 'https://api.openweathermap.org/data/2.5/weather'
